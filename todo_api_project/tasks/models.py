4384cf1dba045303037546d766d86068104345df
LYCE899 <a.coulibaly@alustudent.com>
2024-11-14 02:56:43 +0200

    commit

---
+++ todo_api_project/tasks/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
